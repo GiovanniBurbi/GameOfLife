a2390d39fab5c1a89f7bbf7eb0e0c4950999e388
Giovanni <giovanniburbi10@gmail.com>
2021-12-08 15:23:28 +0100

    Fix: method documentation
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- view/utilities/matrix_board_conversion.py
+++ view/utilities/matrix_board_conversion.py
@@ -6,13 +6,13 @@ def matrix_board_conversion(label, board, px_width, px_height):
     """
     Method for convert an numpy array to a label for the GUI.
     It updates the pixmap of the label passed as parameter to the method and
-    then returns it.
+    then returns it scaled to the pixel dimensions required.
     """
     # Convert the np array in a QImage with RGB channel
     qimage = array2qimage(board)
     # Converts the Qimage in a Pixmap that can be used as a paint device.
     pixmap = QPixmap.fromImage(qimage)
     # Scale the pixmap to the pixel dimensions of the GUI's BoardLayout
-    pixmap = pixmap.scaled(px_width - 1, px_height - 1)
+    pixmap = pixmap.scaled(px_width, px_height)
     label.setPixmap(pixmap)
     return label
