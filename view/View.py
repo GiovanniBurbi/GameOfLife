f5c2a7f5fed0a96ba7b0f5f548679f62dc3faff5
Giovanni <giovanniburbi10@gmail.com>
2021-12-08 14:03:43 +0100

    View delegates all user's interaction with the GUI
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- view/View.py
+++ view/View.py
@@ -29,15 +29,17 @@ class View(QMainWindow):
     def add_board_widget(self, board):
         """ Creates an instance of BoardWidget and adds it
         to the BoardLayout of the user interface """
-        self._board_widget = BoardWidget(board)
+        self._board_widget = BoardWidget(board, self)
         self._ui.BoardLayout.addWidget(self._board_widget)
 
     def set_controller(self, controller):
-        """ Setter of the controller instance.
-        It also calls the setter for the board widget """
+        """ Setter of the controller instance. """
         self._controller = controller
-        self._board_widget.set_controller(controller)
 
     def update_view(self, board):
         """ Delegates to the board widget the update of the board graphic """
         self._board_widget.update_board_state(board)
+
+    def change_cell(self, x, y):
+        """ Delegates to the controller the change in the state of the cell """
+        self._controller.change_state_cell(x, y)
