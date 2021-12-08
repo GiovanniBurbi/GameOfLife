dcd238963f6e94a84279c75d04df6a8c1a62f1a5
Giovanni <giovanniburbi10@gmail.com>
2021-12-08 12:08:23 +0100

    controller setter and update method
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- view/View.py
+++ view/View.py
@@ -6,7 +6,13 @@ from view import BoardWidget, Ui_GameOfLife
 class View(QMainWindow):
     """
     View component of the Model-View-Controller architecture.
-    Extend the GUI implemented with PyQT5 designer in Ui_GameOfLife.py
+    Setup and extends the GUI implemented with PyQT5 designer in Ui_GameOfLife.py
+
+    Attributes:
+        ui : User interface made with PyQt5 Designer. Access to all components of the GUI.
+        board_widget : Custom widget, Graphic represent of the board.
+        controller : Reference to an instance of the controller.
+                     View delegates to this object any user's interaction with the GUI.
     """
 
     def __init__(self):
@@ -14,6 +20,7 @@ class View(QMainWindow):
         self._ui = Ui_GameOfLife()
         self._ui.setupUi(self)
         self._board_widget = None
+        self._controller = None
 
     @property
     def ui(self):
@@ -24,3 +31,13 @@ class View(QMainWindow):
         to the BoardLayout of the user interface """
         self._board_widget = BoardWidget(board)
         self._ui.BoardLayout.addWidget(self._board_widget)
+
+    def set_controller(self, controller):
+        """ Setter of the controller instance.
+        It also calls the setter for the board widget """
+        self._controller = controller
+        self._board_widget.set_controller(controller)
+
+    def update_view(self, board):
+        """ Delegates to the board widget the update of the board graphic """
+        self._board_widget.update_board_state(board)
