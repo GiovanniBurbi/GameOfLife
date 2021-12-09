4f823640c6ea275545d6a733e4c804a4c70c8304
Giovanni <giovanniburbi10@gmail.com>
2021-12-09 15:34:03 +0100

    Method to add a grid over a scene
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- view/utilities/__init__.py
+++ view/utilities/__init__.py
@@ -1,1 +1,2 @@
+from .grid import create_grid
 from .matrix_board_conversion import matrix_board_conversion
---
+++ view/utilities/grid.py
@@ -0,0 +1,22 @@
+from PyQt5.QtCore import Qt, QLineF
+from PyQt5.QtGui import QPen
+from PyQt5.QtWidgets import QGraphicsItemGroup, QGraphicsLineItem
+
+
+def create_grid(width, height, px_width, px_height, scene):
+    """ Method that creates a gray grid as ItemGroup and adds it to a scene """
+    pen = QPen(Qt.gray)
+    grid = QGraphicsItemGroup()
+    for x in range(1, width):
+        xc = x * (px_width / width)
+        line = QGraphicsLineItem(QLineF(xc, 0, xc, px_height))
+        line.setPen(pen)
+        grid.addToGroup(line)
+
+    for y in range(1, height):
+        yc = y * (px_height / height)
+        line = QGraphicsLineItem(QLineF(0, yc, px_width, yc))
+        line.setPen(pen)
+        grid.addToGroup(line)
+
+    scene.addItem(grid)
