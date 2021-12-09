6cf388ea5b8b0fd06a7989997b9a146687c1d6af
Giovanni <giovanniburbi10@gmail.com>
2021-12-09 15:20:31 +0100

    Some changes to the board widget and conversion method
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- view/utilities/matrix_board_conversion.py
+++ view/utilities/matrix_board_conversion.py
@@ -6,7 +6,7 @@ def matrix_board_conversion(label, board, px_width, px_height):
     """
     Method for convert an numpy array to a label for the GUI.
     It updates the pixmap of the label passed as parameter to the method and
-    then returns it scaled to the pixel dimensions required.
+    scales it to the pixel dimensions required.
     """
     # Convert the np array in a QImage with RGB channel
     qimage = array2qimage(board)
@@ -15,4 +15,3 @@ def matrix_board_conversion(label, board, px_width, px_height):
     # Scale the pixmap to the pixel dimensions of the GUI's BoardLayout
     pixmap = pixmap.scaled(px_width, px_height)
     label.setPixmap(pixmap)
-    return label
