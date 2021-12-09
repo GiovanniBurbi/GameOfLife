a255dcbf0094bdc29ec49c52363c434c3f15aab9
Giovanni <giovanniburbi10@gmail.com>
2021-12-09 15:45:35 +0100

    Modifications to View's board widget creation and added call to grid creation
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- view/utilities/__init__.py
+++ view/utilities/__init__.py
@@ -1,2 +1,2 @@
-from .grid import create_grid
+from .grid import create_grid_over_scene
 from .matrix_board_conversion import matrix_board_conversion
--- view/utilities/grid.py
+++ view/utilities/grid.py
@@ -3,7 +3,7 @@ from PyQt5.QtGui import QPen
 from PyQt5.QtWidgets import QGraphicsItemGroup, QGraphicsLineItem
 
 
-def create_grid(width, height, px_width, px_height, scene):
+def create_grid_over_scene(width, height, px_width, px_height, scene):
     """ Method that creates a gray grid as ItemGroup and adds it to a scene """
     pen = QPen(Qt.gray)
     grid = QGraphicsItemGroup()
