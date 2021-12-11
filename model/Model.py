8b24c528dd71bd6aca27313189cb664f61983cb4
Giovanni <giovanniburbi10@gmail.com>
2021-12-11 18:48:04 +0100

    methods to generate next stage of the conway's game of life simulation
    Signed-off-by: Giovanni <giovanniburbi10@gmail.com>

--- model/Model.py
+++ model/Model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.ndimage import convolve
 
 from model import Observable
 
@@ -17,6 +18,7 @@ class Model(Observable):
                 Initial state is a zero matrix, meaning all cells are dead.
         ratio : matrix board ratio
         zoom : zoom of the board selected
+        kernel: convolution kernel. To calculate sum of the values of the adjacent cells
     """
 
     def __init__(self, height=30, width=60):
@@ -24,8 +26,11 @@ class Model(Observable):
         self._width = width
         self._height = height
         self._ratio = int(width / height)
-        self._board = np.zeros((height, width))
+        self._board = np.zeros((height, width), np.int8)
         self._zoom = 0
+        self._kernel = np.array([[1, 1, 1],
+                                 [1, 0, 1],
+                                 [1, 1, 1]])
 
     @property
     def visible_board(self):
@@ -77,5 +82,24 @@ class Model(Observable):
         return pos_x, pos_y
 
     def clear(self):
+        """ Method that set the board to the initial state of all cells dead. """
         self._board = np.zeros((self._height, self._width))
+        self.value = self.visible_board
+
+    def _compute_neighbors(self):
+        """ Method to calculate, for every cell, how many adjacent alive
+        cells they have """
+        return convolve(self._board, self._kernel, mode="constant", cval=0)
+
+    def next_generation(self):
+        """ Method to generate the next state of the board according
+        to Conway's Game Of Life rules. """
+        neighbors = self._compute_neighbors()
+        # Using numpy mechanism to make logical operations element-wise between
+        # the matrix of the current state and the matrix of neighbors. Then
+        # in the resulting matrix, representing the next state, False is converted
+        # in 0, dead cell, and True is converted in 1, alive cell.
+        self._board = (
+                ((self._board == 1) & (neighbors > 1) & (neighbors < 4))
+                | ((self._board == 0) & (neighbors == 3))).astype(np.uint8)
         self.value = self.visible_board
